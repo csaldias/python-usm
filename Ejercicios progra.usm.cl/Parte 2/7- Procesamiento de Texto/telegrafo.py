829cb0a2c63ec9a7e82027ef2b02a0178d8f79f6
Camilo Saldias <camilo.saldias@gmail.com>
2014-11-11 16:23:12 -0300

    Correcciones varias

--- Ejercicios progra.usm.cl/Parte 2/7- Procesamiento de Texto/telegrafo.py
+++ Ejercicios progra.usm.cl/Parte 2/7- Procesamiento de Texto/telegrafo.py
@@ -2,14 +2,16 @@
 msj = raw_input("Mensaje: ").lower()
 costo = 0
 
-for letra in msj:
-    if letra != ' ':
-    	#if letra in 'qwertyuiopasdfghjklzxcvbnm'
-        if ord(letra) in range(97,123):
-            costo += 10
-        #elif letra in '1234567890'
-        elif ord(letra) in range(65,91):
-            costo += 20
-        else:
-            costo += 30
+#Quitamos los saltos de linea y los espacios del mensaje
+msj_filtro = msj.replace(" ","").replace("\n","")
+
+for letra in msj_filtro:
+   	#if letra in 'qwertyuiopasdfghjklzxcvbnm'
+    if ord(letra) in range(97,123):
+        costo += 10
+    #elif letra in '1234567890'
+    elif ord(letra) in range(48,58):
+        costo += 20
+    else:
+        costo += 30
 print "Su mensaje cuesta $" + str(costo)
